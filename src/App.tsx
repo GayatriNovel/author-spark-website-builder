cdf17fddfde284a787ee18f9761f4ec002191c06
GayatriNovel <gayatrinsarkar@gmail.com>
2025-09-09 15:21:04 +1000

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
+import GoodreadsReviews from "./GoodreadsReviews";  // ✅ fixed import (capitalized + plural)
 
 const queryClient = new QueryClient();
 
@@ -13,6 +14,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
+      <GoodreadsReviews />   {/* ✅ matches import */}
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
