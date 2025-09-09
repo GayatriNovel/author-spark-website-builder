b4e04335900ecc8c8e640a75036215a30bc3a51d
GayatriNovel <gayatrinsarkar@gmail.com>
2025-09-09 20:54:02 +1000

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -5,14 +5,17 @@ import Contact from "@/components/Contact";
 import Footer from "@/components/Footer";
 import GoodreadsReviews from "@/components/GoodreadsReviews";
 
-
 const Index = () => {
   return (
-    <div className="min-h-screen">
+    <div className="min-h-screen flex flex-col">
       <Hero />
       <FeaturedBooks />
       <About />
       <Contact />
+
+      {/* Goodreads Reviews at the bottom */}
+      <GoodreadsReviews />
+
       <Footer />
     </div>
   );
