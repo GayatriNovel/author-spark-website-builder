faafdb023ad4460240a8cea2ecf0849319585dac
GayatriNovel <gayatrinsarkar@gmail.com>
2025-09-23 11:19:33 +1000

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,14 @@
+// src/App.tsx
+import React from "react";
 import Events from "./pages/events";
-
 
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom"; // NOTE: no BrowserRouter here
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
-//import GoodreadsReviews from "./components/GoodreadsReviews";  // ✅ fixed import (capitalized + plural)
-// ...
-<Routes>
-  {/* other routes */}
-  <Route path="/events" element={<Events />} />
-</Routes>
 
 const queryClient = new QueryClient();
 
@@ -22,16 +17,15 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-          <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Index />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
+      {/* Router is provided in main.tsx via <BrowserRouter> */}
+      <Routes>
+        <Route path="/" element={<Index />} />
+        <Route path="/events" element={<Events />} />
+        {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+        <Route path="*" element={<NotFound />} />
+      </Routes>
     </TooltipProvider>
   </QueryClientProvider>
 );
 
 export default App;
-
